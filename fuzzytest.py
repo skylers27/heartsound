f147b9f091c372b672c14a3d7f3c980517d6666b
Alisa Levin <alisalevin.nyc@gmail.com>
2018-10-23 21:44:52 -0400

    fuzzytest

--- fuzzytest.py
+++ fuzzytest.py
@@ -17,22 +17,48 @@ sigmas = [[0.8, 0.3],
           [0.3, 0.5],
           [1.1, 0.7]]
 
-# Generate test data
-np.random.seed(42)  # Set seed for reproducibility
-xpts = np.zeros(1) # x[all rows][0]
-ypts = np.zeros(1) # x[all rows][1]
-labels = np.zeros(1) # no labels 
-for i, ((xmu, ymu), (xsigma, ysigma)) in enumerate(zip(centers, sigmas)):
-    xpts = np.hstack((xpts, np.random.standard_normal(200) * xsigma + xmu))
-    ypts = np.hstack((ypts, np.random.standard_normal(200) * ysigma + ymu))
-    labels = np.hstack((labels, np.ones(200) * i))
+# Insert his test data instead !!!!
+# Then our data 
+
+# Collect Test Data 
+with open("testFun.dat") as textFile:
+    y = [line.split() for line in textFile]
+
+y = np.array(y)
+X = np.zeros(shape=(200,2))
+
+# stores test data as number in array X (converts from strings)
+for i in range(0,len(y)): # num rows 
+  for j in range(0,len(y[0])): # num columns 
+    X[i,j] = float(y[i,j])
+   
+xpts = np.zeros(len(y[0])) 
+ypts = np.zeros(len(y[0])) 
+labels = np.zeros(len(y[0])) # no labels 
+
+# which are columns and rows 
+print ("len(y[0])", len(y[0]))
+print ("len(y)", len(y))
+
+
+# xpts = x[all rows][0]
+for i in range (0, len(y[0])):
+  xpts[i] = X[i][0]
+
+# ypts = x[all rows][1]
+for i in range (0, len(y[0])):
+  ypts[i] = X[i][1]
+
+print ("LABELS\n")
+for i in range (len(labels)): # should be way more like in fuzzy.py
+  print (labels[i])
 
 # Visualize the test data
 fig0, ax0 = plt.subplots()
-for label in range(3):
+for label in range(2): # need 2 different kinds of labels, only have 1 cuz theyre not labeled...
     ax0.plot(xpts[labels == label], ypts[labels == label], '.',
              color=colors[label])
-ax0.set_title('Test data: 200 points x3 clusters.')
+ax0.set_title('Test data: 200 points x2 clusters.')
 plt.show()
 
 # Set up the loop and plot
@@ -43,7 +69,12 @@ fpcs = []
 for ncenters, ax in enumerate(axes1.reshape(-1), 2):
     cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
         alldata, ncenters, 2, error=0.005, maxiter=1000, init=None)
-    print('Centers = ', str(ncenters), '\n', u0) # u0 is the array of the memberiship functions 
+    print("Centers = ", str(ncenters), "\n") # u0 is the array of the memberiship functions 
+    print ("Data\t\t Cluster 1\t\t Cluster 2\n") 
+    for i in range (len(y[0])): # columns
+      print (xpts[i], ",", ypts[i])
+      for j in range (ncenters): # rows
+        print ("\t\t ", u0[i][j]) 
 
     # Store fpc values for later
     fpcs.append(fpc)
